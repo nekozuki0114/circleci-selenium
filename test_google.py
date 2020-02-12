3387cf0c497f317d2e6afa0d6db133c9ec3dee7a
nekozuki0114 <nekozuki.090@gmail.com>
2020-02-12 17:02:15 +0900

    fixtest

--- test_google.py
+++ test_google.py
@@ -22,7 +22,7 @@ class TestGoogle:
 
         driver.save_screenshot("test-reports/result_002.png")
 
-        driver.find_element_by_css_selector("#tsf > div:nth-child(2) > div > div.FPdoLc.VlcLAe > center > input.gNO89b").click()
+        driver.find_element_by_css_selector("#tsf > div:nth-child(2) > div > div.FPdoLc tfB0Bf > center > input.gNO89b").click()
 
         driver.save_screenshot("test-reports/result_003.png")
 
