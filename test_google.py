388cabfceea8a4d0f751d042b78e4ccda4ebdbda
nekozuki0114 <nekozuki.090@gmail.com>
2020-02-28 17:51:47 +0900

    test

--- test_google.py
+++ test_google.py
@@ -1,6 +1,5 @@
 import pytest
-import time
-from selenium imaport webdriver
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 class TestGoogle:
@@ -13,7 +12,6 @@ class TestGoogle:
 
     def setup_method(self):
         self.driver.get("http://example123123123.com/vaddy-f5b30903bb749fe3447b.html")
-   time.sleep(30)
     def test_case(self):
         driver = self.driver
 
